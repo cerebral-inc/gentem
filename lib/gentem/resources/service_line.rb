c8ee1ca2747e2f448c680300fe4378bd3cecec08
Kevin J. Storberg <kevin@storberg.net>
2020-10-28 17:09:44 -0700

    specs

--- lib/gentem/resources/service_line.rb
+++ lib/gentem/resources/service_line.rb
@@ -1,14 +1,18 @@
 module Gentem
   module Resources
-    class ServiceLine < RestfulRequest
+    class ServiceLine < RestfulResource
       public :list, :create, :read, :replace, :update, :destroy
 
       def initialize(appointment_id)
-        @appointment_id
+        @appointment_id = appointment_id
       end
 
+      private
+
+      attr_reader :appointment_id
+
       def resource_base
-        "appointments/#{@appointment_id}/service_lines/"
+        "appointments/#{appointment_id}/service_lines/"
       end
     end
   end
