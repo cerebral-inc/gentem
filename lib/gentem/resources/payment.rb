a3fc8a1a83365310ba92be2033d3c4a6a945d76e
Konstantin Rafalski <kraft001@gmail.com>
2020-11-24 12:52:46 +0300

    Update the payments endpoint

--- lib/gentem/resources/payment.rb
+++ lib/gentem/resources/payment.rb
@@ -3,16 +3,10 @@ module Gentem
     class Payment < RestfulResource
       public :list, :create, :read
 
-      def initialize(appointment_id)
-        @appointment_id = appointment_id
-      end
-
       private
 
-      attr_reader :appointment_id
-
       def resource_base
-        "appointments/#{appointment_id}/patient_payments/"
+        'patient_payments'
       end
     end
   end
--- spec/resources/payment_spec.rb
+++ spec/resources/payment_spec.rb
@@ -1,8 +1,8 @@
 RSpec.describe Gentem::Resources::Payment do
   include_context 'with sandbox configuration'
 
-  let(:resource_base_path) { "appointments/appointment_id/patient_payments/" }
-  let(:arguments) { ['appointment_id'] }
+  let(:resource_base_path) { "patient_payments" }
+  let(:arguments) { [] }
 
   it_behaves_like 'a read action'
   it_behaves_like 'a list action'
